33eaa89a982c8668c01ce54104e34f004c6a8a4d
Tarak698 <tarakpatra18@gmail.com>
2023-04-15 16:36:02 +0530

    second

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -18,24 +18,43 @@ router.route('/').get((req, res) => {
     res.send('Hello from DALL-E!');
 });
 
-router.route('/').post(async (req, res) => {
-    try {
-      const { prompt } = req.body;
+// router.route('/').post(async (req, res) => {
+//     try {
+//       const { prompt } = req.body;
   
-      const aiResponse = await openai.createImage({
-        prompt,
-        n: 1,
-        size: '1024x1024',
-        response_format: 'b64_json',
+//       const aiResponse = await openai.createImage({
+//         prompt,
+//         n: 1,
+//         size: '1024x1024',
+//         response_format: 'b64_json',
+//       });
+  
+//       const image = aiResponse.data.data[0].b64_json;
+//       res.status(200).json({ photo: image });
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).send(error?.response.data.error.message);
+//     }
+//   });
+
+      router.route('/').post(async (req, res) => {
+        try {
+          const { prompt } = req.body;
+      
+          const aiResponse = await openai.createImage({
+            prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json',
+          });
+      
+          const image = aiResponse.data.data[0].b64_json;
+          res.status(200).json({ photo: image });
+        } catch (error) {
+          console.error(error);
+          res.status(500).send(error?.response.data.error.message);
+        }
       });
-  
-      const image = aiResponse.data.data[0].b64_json;
-      res.status(200).json({ photo: image });
-    } catch (error) {
-      console.error(error);
-      res.status(500).send(error?.response.data.error.message);
-    }
-  });
 
 export default router;
 
